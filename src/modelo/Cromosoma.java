49c351e5f390707255f80e66b543471b38e3951e
Josepe04 <Josepe04@ucm.es>
2017-03-06 17:57:14 +0100

    la clase abstracta cromosoma ya esta como debe

--- src/modelo/Cromosoma.java
+++ src/modelo/Cromosoma.java
@@ -1,55 +1,11 @@
 package modelo;
 
-public class Cromosoma {
-	private boolean[] genes;
-	private double fenotipo;
-	private double aptitud;
-	private double puntuacion;
-	private double punt_acum;
-	
-	public Cromosoma(boolean[] gen){
-		genes = gen;
-	}
+public abstract class Cromosoma {
 	
 	
-	public double evalua(){
-		return 0;
-	}
-	
-	public double getFenotipo() {
-		return fenotipo;
-	}
-	public void setFenotipo(double fenotipo) {
-		this.fenotipo = fenotipo;
-	}
-	public double getAptitud() {
-		return aptitud;
-	}
-	public void setAptitud(double aptitud) {
-		this.aptitud = aptitud;
-	}
-	public double getPuntuacion() {
-		return puntuacion;
-	}
-	public void setPuntuacion(double puntuacion) {
-		this.puntuacion = puntuacion;
-	}
-	public double getPunt_acum() {
-		return punt_acum;
-	}
-	public void setPunt_acum(double punt_acum) {
-		this.punt_acum = punt_acum;
-	}
-
-	private int bin2dec(){
-            int ret = 0;
-            for(int i = 0; i < genes.length;i++){
-                if(genes[i]){
-                    ret += Math.pow(2, i);
-                }
-            }
-            return ret;
-        } 
-        
-	
+	abstract double evalua();
+	abstract double fenotipoenotipo();
+	abstract double getAptitud();
+	abstract double getPuntuacion();
+	abstract void setPuntuacion(double puntuacion);
 }
