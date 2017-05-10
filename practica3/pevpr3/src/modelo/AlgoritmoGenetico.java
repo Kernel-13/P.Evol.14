d2940b9e68c5c1dcff6df45e0c52ecefd10da8b4
Josepe04 <Josepe04@ucm.es>
2017-05-10 12:07:09 +0200

    inicializacionCreciente

--- practica3/pevpr3/src/modelo/AlgoritmoGenetico.java
+++ practica3/pevpr3/src/modelo/AlgoritmoGenetico.java
@@ -8,6 +8,7 @@ package modelo;
 import java.util.ArrayList;
 import java.util.Random;
 import util.DatosGrafica;
+import util.Nodo;
 import util.TipoCruce;
 import util.TipoFuncion;
 import util.TipoMutacion;
@@ -19,8 +20,9 @@ import util.TipoSeleccion;
  * @author josemanuel
  */
 public class AlgoritmoGenetico {
+
     public static double ELITE = 0.05;
-    
+
     private int tamPoblacion;
     private int iteraciones;
     private double probCruces;
@@ -36,57 +38,60 @@ public class AlgoritmoGenetico {
     private int[][] d;
     private boolean inv;
     private int maxDepth;
+    private int numTerminales;
     Factoria f;
-    
+
     public AlgoritmoGenetico(TipoFuncion funcion, int tampob, int iteraciones,
             double probCruces, double probMutacion, double precision,
-            TipoSeleccion tSeleccion,int nvars,boolean elitismo,int[][]f2,
-            int[][]d,TipoCruce c,TipoMutacion m,boolean inv){
-        
+            TipoSeleccion tSeleccion, int nvars, boolean elitismo, int[][] f2,
+            int[][] d, TipoCruce c, TipoMutacion m, boolean inv) {
+
         tamPoblacion = tampob;
         this.iteraciones = iteraciones;
-        this.probCruces = probCruces/100;
-        this.probMutacion = probMutacion/100;
+        this.probCruces = probCruces / 100;
+        this.probMutacion = probMutacion / 100;
         this.precision = precision;
         this.nvars = nvars;
         this.elitismo = elitismo;
-        f = new Factoria(funcion,tSeleccion);
+        f = new Factoria(funcion, tSeleccion);
         seleccion = f.factoriaSeleccion();
         //problema = f.factoriaProblema(nvars);
         tamElite = calcularTamElite();
         this.f2 = f2;
         this.d = d;
         this.inv = inv;
-        problema = new Problema(c,m,f2,d);
+        problema = new Problema(c, m, f2, d);
+        numTerminales = 6;
     }
-    
+
     /**
-     * funcion que calcula el tamanio que debe tener la elite
-     * teniendo en cuenta el porcentaje de la poblacion 
-     * que forma parte de la elite
-     * @return 
+     * funcion que calcula el tamanio que debe tener la elite teniendo en cuenta
+     * el porcentaje de la poblacion que forma parte de la elite
+     *
+     * @return
      */
-    private int calcularTamElite(){
-        double aux = tamPoblacion*ELITE;
-        int ret = (int)aux;
-        if(ret <= 0 || ret > tamPoblacion){
+    private int calcularTamElite() {
+        double aux = tamPoblacion * ELITE;
+        int ret = (int) aux;
+        if (ret <= 0 || ret > tamPoblacion) {
             elitismo = false;
         }
         return ret;
     }
-    
+
     /**
      * algoritmo genetico
+     *
      * @param semilla
-     * @return 
+     * @return
      */
-    public DatosGrafica ejecuta(int semilla){
+    public DatosGrafica ejecuta(int semilla) {
         ArrayList<Double> best = new ArrayList<>();
         ArrayList<Double> bestPob = new ArrayList<>();
         ArrayList<Double> media = new ArrayList<>();
         CromosomaAsigC mejorPob;
         pobInicial(semilla);
-        if(this.elitismo){
+        if (this.elitismo) {
             problema.iniElite(pob, tamElite);
             problema.elitismo(pob, tamElite);
         }
@@ -94,13 +99,14 @@ public class AlgoritmoGenetico {
         bestPob.add(mejorPob.getAptitud());
         best.add(problema.getBest().getAptitud());
         media.add(problema.media(tamPoblacion));
-        for(int i=1; i < iteraciones;i++){
+        for (int i = 1; i < iteraciones; i++) {
             pob = seleccion.selecciona(pob);
             problema.reproduccion(pob, probCruces);
             problema.mutacion(pob, probMutacion);
-            problema.mutacionInversion(pob,probMutacion, inv);
-            if(this.elitismo)
+            problema.mutacionInversion(pob, probMutacion, inv);
+            if (this.elitismo) {
                 problema.elitismo(pob, tamElite);
+            }
             mejorPob = problema.evaluacion(pob);
             bestPob.add(mejorPob.getAptitud());
             best.add(problema.getBest().getAptitud());
@@ -111,58 +117,88 @@ public class AlgoritmoGenetico {
 //            System.out.println(c.toString());
 //        }
         System.out.println(problema.getBest().toString());
-        return new DatosGrafica(best,iteraciones,bestPob,media,problema.getBest());
+        return new DatosGrafica(best, iteraciones, bestPob, media, problema.getBest());
     }
-    
+
     /**
      * funcion que genera la población inicial
-     * @param semilla 
+     *
+     * @param semilla
      */
-    private void pobInicial(int semilla){
+    private void pobInicial(int semilla) {
         boolean repetidos = false;
         Random r = new Random(semilla);
         pob = new CromosomaAsigC[tamPoblacion];
-        if(factorial(nvars)>pob.length)
+        if (factorial(nvars) > pob.length) {
             repetidos = true;
-        
-        for(int i = 0; i < tamPoblacion;i++){
+        }
+
+        for (int i = 0; i < tamPoblacion; i++) {
             pob[i] = new CromosomaAsigC(nvars);
-            pob[i].inicializa(r,f2,d);
-            if(containsLista(pob[i],i-1)){
-                pob[i].inicializa(r,f2,d);
+            pob[i].inicializa(r, f2, d);
+            if (containsLista(pob[i], i - 1)) {
+                pob[i].inicializa(r, f2, d);
             }
         }
-        
-    }
-    
-    private void inicioCompleto(int depth){
-        if(depth < this.maxDepth){
-            // pilla funcion aleatoria
-        }
+
     }
 
     private TipoOperacion randomFunction() {
         int pick = new Random().nextInt(TipoOperacion.values().length);
         return TipoOperacion.values()[pick];
     }
+
     
-    private boolean containsLista(CromosomaAsigC elem,int n){
+    private Nodo inicioCreciente(int depth) {
+        Random r = new Random();
+        TipoOperacion op;
+        op = randomFunction();
+        if(depth >= this.maxDepth){
+            op = TipoOperacion.HOJA;
+        }
+        switch (op) {
+            case AND:
+                return new Nodo(op, 0, inicioCreciente(depth + 1), inicioCreciente(depth + 1), null);
+            case OR:
+                return new Nodo(op, 0, inicioCreciente(depth + 1), inicioCreciente(depth + 1), null);
+            case NOT:
+                return new Nodo(op, 0, inicioCreciente(depth + 1), null, null);
+            case IF:
+                return new Nodo(op, 0, inicioCreciente(depth + 1), inicioCreciente(depth + 1), inicioCreciente(depth + 1));
+            default:
+                return new Nodo(op, r.nextInt(numTerminales), null, null, null);
+        }
+    }
+    
+    /*private Nodo inicioRampedAndHalf(boolean modo,int tamGrupo,int depth){
+        if(modo && depth%tamGrupo == 0)
+            modo = !modo;
+        if(modo)
+            return inicioCreciente(depth);
+        else
+            return inicioCompleto(depth);
+    }*/
+
+    private boolean containsLista(CromosomaAsigC elem, int n) {
         boolean ret = true;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < elem.getTamanio();j++){
-                if(pob[i].getGenes().get(j) != elem.getGenes().get(j))
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < elem.getTamanio(); j++) {
+                if (pob[i].getGenes().get(j) != elem.getGenes().get(j)) {
                     ret = false;
+                }
             }
-            if(ret)
+            if (ret) {
                 return true;
+            }
         }
         return false;
     }
-    
-    public int factorial (int numero) {
-        if (numero==0)
-          return 1;
-        else
-          return numero * factorial(numero-1);
+
+    public int factorial(int numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
     }
 }
